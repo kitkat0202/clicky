6de46924f9f5dcba7ff62fb890a60087269af767
KatHe <he.katherine321@gmail.com>
2018-10-27 00:26:46 -0400

    gh-pages

---
+++ precache-manifest.e688eb638b1b93278f0c6cf0b7e3b741.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e53dddbe16ca2509119",
+    "url": "/clicky/static/js/runtime~main.9e53dddb.js"
+  },
+  {
+    "revision": "61c64e582d475f01a5b1",
+    "url": "/clicky/static/js/main.61c64e58.chunk.js"
+  },
+  {
+    "revision": "ace62ad78903889e8e0e",
+    "url": "/clicky/static/js/1.ace62ad7.chunk.js"
+  },
+  {
+    "revision": "61c64e582d475f01a5b1",
+    "url": "/clicky/static/css/main.553cfc65.chunk.css"
+  },
+  {
+    "revision": "33b8d6764db94fe0f899f64fc3bafbea",
+    "url": "/clicky/index.html"
+  }
+];
